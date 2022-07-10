fa1e57eecfabfd2ef394ba1527d0cb4af275dd30
YeeticusFinch <50182007+YeeticusFinch@users.noreply.github.com>
2022-07-10 12:33:12 +0200

    Screw you orbital mechanics

--- src/com/lerdorf/voidships/Orbit.java
+++ src/com/lerdorf/voidships/Orbit.java
@@ -49,7 +49,6 @@ class Orbit {
 	public double period; // hours per year
 	public double[] axis; // [x, y, z] unit vector
 	public double offset; // radians
-
 	public static final int DIRECT = 0;
 	public static final int OBERTH = 1;
 	public static final int HOHMANN = 2;
@@ -69,7 +68,57 @@ class Orbit {
 		calculatePeriod();
 	}
 	
-	// [deltaV, total time, time before launch, acceleration time, cruising time, acceleration time]
+	// Travel to a new offset within the same orbit
+	// [total deltaV, total time, acceleration time, cruising time, acceleration time]
+	public double[] deltaVOrbitTraverse(double newOffset, double acceleration) { // acceleration in m/s^s
+		// g = w*w * r
+		// a = @ * r
+		// Each orbit has a certain velocity required to maintain the orbit
+		// If the velocity is too small, the ship will fall towards the planet
+		// If the velocity is too large, the ship will fall away from the planet
+		// The only way to allow for a different velocity at that orbit radius is to change the acceleration
+		// The fastest a spaceship can travel around an orbit will be when the spaceship is pointing towards the celestial body with full thrust
+		// The slowest is when the spaceship is pointing away from the celestial body and directly countering the gravity
+		// Because of this, the ship can't do maximum acceleration, it averages to about half, so that's what we'll use
+		// g + t = w*w * r
+		// w = sqrt( (g + t) / r ) // Max angular velocity
+		// w = Max(0, sqrt( g - t ) / r) // Min angular velocitty
+		acceleration *= 3600*3600 / 1000; // convevrt to km / h^2 
+		double alpha = 0.5 * acceleration / radius; // average angular acceleration
+		double dist = angleDiff2(newOffset, offset); // shortest path
+		double g = body.gravAccel(radius) * 3600*3600 / 1000; // gravity in km / h^2
+		double w = Math.sqrt(g/radius); // current rotational velocity (rad/h)
+		double maxW = Math.sqrt(g + acceleration) / radius; // maximum rotational velocity (rad/h)
+		double accelTime = (maxW - w) / alpha; // hours
+		double accelDist = 0.5*(maxW - w) * accelTime; // angular distance traveled during that time
+		if (2*accelDist >= Math.abs(dist)) { // can't accelerate to full speed, distances are too close
+			double fraction = dist / (2*accelDist);
+			accelTime *= fraction;
+			return new double[] {2*fraction*Math.abs(maxW-w), 2*accelTime, accelTime, 0, accelTime};
+		}
+		double cruiseDist = Math.abs(dist) - 2*accelDist;
+		double cruiseTime = 0;
+		if (Math.signum(dist) == Math.signum(axis[2])) // same direction
+			cruiseTime = cruiseDist / (maxW - w);
+		else
+			cruiseTime = cruiseDist / (maxW + w); // opposite direction
+		return new double[] { 2*Math.abs(maxW-w), 2*accelTime+cruiseTime, accelTime, cruiseTime, accelTime };
+	}
+	
+	// Changes the direction of the orbit
+	// [deltaV, time]
+	public double[] deltaVSetDirection(int dir, double acceleration) {
+		if (Math.signum(dir) == Math.signum(axis[2]))
+			return new double[] {0,0};
+		double alpha = 0.5 * acceleration / radius;
+		double g = body.gravAccel(radius) * 3600*3600 / 1000;
+		double w = Math.sqrt(g/radius);
+		double accelTime = 2*w/alpha;
+		return new double[] {2*w, accelTime};
+	}
+	
+	// time is in hours, speed is in km/h
+	// [total deltaV, total time, time before launch, acceleration time, cruising time, acceleration time]
 	public double[] deltaV(Orbit o, double acceleration, int transferType) { // acceleration in m/s^2
 		Orbit primaryOrbit = this;
 		Orbit secondaryOrbit = o;
@@ -104,7 +153,7 @@ class Orbit {
 	}
 	
 	public void calculatePeriod() {
-		period = 2*Math.PI / Math.sqrt( body.gravAccel(radius)*3.6 / radius );
+		period = 2*Math.PI / Math.sqrt( body.gravAccel(radius)*3.6 / (radius*1000) );
 	}
 	
 	public double angleDiff(double target, double current) {
@@ -113,6 +162,14 @@ class Orbit {
 		else if (current > target)
 			return 2*Math.PI - current % (Math.PI*2) + target;
 		else return 0;
+	}
+	
+	public double angleDiff2(double target, double current) {
+		double a = target - current;
+		double b = 2*Math.PI - current % (Math.PI*2) + target;
+		if (Math.abs(a) < Math.abs(b))
+			return a;
+		return b;
 	}
 	
 	public double angularDiff(Orbit o) {
@@ -135,7 +192,7 @@ class Orbit {
 	 */
 	// Gets angular speed of this orbit (in radians per hour)
 	public double getAngularSpeed() {
-		//return Math.sqrt( body.gravAccel(radius)*3.6 / radius );
+		//return Math.sqrt( body.gravAccel(radius)*3.6 / (radius*1000) );
 		return 2*Math.PI / period;
 	}
 	
